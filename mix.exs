6f85805eaf09347089be2f75e755d098bed02bfc
Po Chen <chenpaul914@gmail.com>
2025-12-28 16:24:57 +1100

    feat: implement IMAP APPEND command

--- mix.exs
+++ mix.exs
@@ -10,6 +10,7 @@ defmodule Blop.Mixfile do
       app: :blop,
       version: @version,
       elixir: "~> 1.14",
+      elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       package: package(),
@@ -23,6 +24,9 @@ defmodule Blop.Mixfile do
       docs: docs()
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp package do
     [
