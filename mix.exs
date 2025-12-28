bd8295ff75bd4a07f4e801053b0adf6b7d1ef13d
Po Chen <chenpaul914@gmail.com>
2025-12-28 20:17:06 +1100

    bump to elixir 1.17

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Blop.Mixfile do
     [
       app: :blop,
       version: @version,
-      elixir: "~> 1.14",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
